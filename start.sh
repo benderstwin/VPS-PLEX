6d179e039a0acc99b9afd8a4152cf4d6fdd168a3
benderstwin <bryan@bwirth.com>
2018-11-23 08:59:33 -0700

    added portainer.yml

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt update && apt upgrade -y
+cd /
+mkdir docker
+cd docker
+
+apt install -y \
+ apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    mc \
+    nano \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update && apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+mkdir apps
